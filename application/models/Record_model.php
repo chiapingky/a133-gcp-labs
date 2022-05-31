58f2c803a695c123fa91593f79ba3e8b419a5cb4
chiapingky <pingky99@gmail.com>
2022-05-31 21:34:06 +0000

    personal info

--- application/models/Record_model.php
+++ application/models/Record_model.php
@@ -8,7 +8,7 @@ class Record_model extends CI_Model {
     public function __construct() {
         $this->client = new Client([
             // TODO: Tambahkan Base URL API
-            'base_uri' => "base_url_backend_Anda",
+            'base_uri' => "http://backend-dot-submission-mgce-chiapingky.et.r.appspot.com",
         ]);
     }
 
